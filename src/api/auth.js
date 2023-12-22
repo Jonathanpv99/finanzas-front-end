b8c83590cdedb9eef0486ca423a4e8eda4c66ca6
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-12-22 14:04:53 -0600

    fix: forbiden login

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,6 @@
 import axios from "./axios";
 
-export const LoginRequest = ( rfc ) => axios.post(`/login`, rfc );
+
+axios.defaults.headers.post['X-CSRFToken'] = 'Gt7aOVqk4OpcRHfcNMmf2HRSn4CbBLHYLo7vUVz99UYM30kIxvU6et6pgnwsw5Z2';
+
+export const LoginRequest = ( rfc ) => axios.post(`/login/`,{rfc: rfc});
